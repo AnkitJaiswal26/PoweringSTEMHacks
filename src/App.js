6d97c183cb81bd09340882a3bcc1e3dbc187d1cb
Amey112 <67439149+Amey112@users.noreply.github.com>
2022-12-29 15:06:54 +0530

    Created login and register page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./App.css"
 import Button from '@mui/material/Button';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegistrationScreen from './screens/RegistrationScreen';
+
 
 
 const App = () => {
+  const [user, setUser] = useState();
+  const handleUser = (userObj) => {
+    setUser(userObj);
+  }
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomeScreen user={user} hadleUser={handleUser}/>,
+    },
+    {
+      path: "/register",
+      element: <RegistrationScreen />,
+    },
+    {
+      path: "/login",
+      element: <LoginScreen />,
+    },
+    {
+      path:"/profile",
+      element: <h1>Profile</h1>
+    }
+  ]);
   return (<>
-    <Button variant="contained" color='green' >Hello</Button>
+     <RouterProvider router={router} />
   </>);
 }
 
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { ThemeProvider } from "@mui/material/styles";
 import theme from "./Theme";
 import { EHRProvider } from "./Context/EHRContext";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
 	<React.StrictMode>
