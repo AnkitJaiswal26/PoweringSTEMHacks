be65cde5b4b735a6fc7b14c9de2daa9c7069b55f
PranitRohokale <pranitrohokale@gmail.com>
2022-12-31 15:54:41 +0530

    org-coloring completed!

--- src/components/HospitalSidebar/HSidebar.jsx
+++ src/components/HospitalSidebar/HSidebar.jsx
@@ -4,31 +4,29 @@ import { hosSidebar } from "./hosSidebar";
 import styles from "./Sidebar.module.css";
 
 const HSidebar = ({ value }) => {
-	return (
-		<div className={styles.sidebar_wrapper}>
-			<div className={styles.logoDiv}>EHRecord</div>
-			<hr className="bg-gray-200 border-0" />
+  return (
+    <div className={styles.sidebar_wrapper}>
+      <Link to="/">
+        <div className={styles.logoDiv}>EHRecord</div>
+      </Link>
+      <hr className="bg-gray-200 border-0" />
 
-			<div>
-				<ul className={styles.sidebarList}>
-					{hosSidebar.map((item, index) => {
-						return (
-							<li key={item.name}>
-								<div
-									className={`${
-										item.name === value ? styles.active : ""
-									}`}
-								>
-									{item.icon}
-									<Link to={item.url}>{item.name}</Link>
-								</div>
-							</li>
-						);
-					})}
-				</ul>
-			</div>
-		</div>
-	);
+      <div>
+        <ul className={styles.sidebarList}>
+          {hosSidebar.map((item, index) => {
+            return (
+              <li key={item.name}>
+                <div className={`${item.name === value ? styles.active : ""}`}>
+                  {item.icon}
+                  <Link to={item.url}>{item.name}</Link>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default HSidebar;
