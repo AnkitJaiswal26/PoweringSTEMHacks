40d1304200790f4b65af34b7cc7d42fa8d1bcd27
PranitRohokale <pranitrohokale@gmail.com>
2022-12-31 16:31:57 +0530

    hospital-Recoloring Completed!!

--- src/screens/Hospital/NewRecord/NewRecord.jsx
+++ src/screens/Hospital/NewRecord/NewRecord.jsx
@@ -96,7 +96,7 @@ const NewRecord = () => {
 				closeModal={closeModal}
 				modalIsOpen={modalIsOpen}
 			/>
-			<HSidebar value="NewRecord" />
+			<HSidebar value="New Record" />
 
 			<div className={styles.main_wrapper}>
 				<div className={styles.navBar}>
@@ -156,7 +156,7 @@ const NewRecord = () => {
 												)
 											}
 											placeholder="Enter Patient address"
-											className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+											className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
 										/>
 									</div>
 								</div>
@@ -178,7 +178,7 @@ const NewRecord = () => {
 											onChange={(e) =>
 												setDocName(e.target.value)
 											}
-											className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+											className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
 										/>
 									</div>
 								</div>
@@ -195,7 +195,7 @@ const NewRecord = () => {
 											type="file"
 											id="formFile"
 											onChange={handlePosterFileChange}
-											className="form-control block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+											className="form-control block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
 										/>
 									</div>
 								</div>
@@ -217,7 +217,7 @@ const NewRecord = () => {
 											onChange={(e) =>
 												setTestSuggested(e.target.value)
 											}
-											className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+											className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
 										/>
 									</div>
 								</div>
@@ -226,7 +226,7 @@ const NewRecord = () => {
 									<button
 										type="submit"
 										onClick={handleSubmit}
-										className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+										className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
 									>
 										Create
 									</button>
